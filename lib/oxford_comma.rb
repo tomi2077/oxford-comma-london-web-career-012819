97b455f58b4617f47b7da0165971460991c6313a
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-17 01:09:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array.join
+  elsif array.count == 2
+    return array.join(" and ")
+  elsif array.count >= 3
+    more_than_3 = array.join(", ")
+    more_than_3.tap {|i| i[i.rindex(", "), 2] = ", and "}
+    return more_than_3
+  end
 end
+
+
