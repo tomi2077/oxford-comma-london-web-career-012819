c197f56780a7410e0762bb081b0b894d49e9f2ce
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-17 00:52:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.count == 2
     return array.join(" and ")
   elsif array.count >= 3
-    more_than_3 = array.join(", ")
+    more_than_3 = array.join(", ").insert("and, )
     
     return array.join(",")
   end
