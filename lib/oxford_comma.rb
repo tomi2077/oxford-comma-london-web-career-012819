105b25ecfa13326b41c211de3fe85608f8c5ff6e
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-17 00:41:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   if array.count == 1
     return array.join
-  array.join(", ")
+  elsif array.count == 2
+    return array.join(" and ")
+  else
+    return array.join()
 end
 
 
